2d90b05372d03c69e4c137fa3f7ef5d793921aca
Guy Luz <guyluz11@gmail.com>
2019-06-05 19:38:06 +0300

    Command function

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -1,25 +1,34 @@
+import 'dart:io';
+import 'package:firebase/firebase_io.dart';
 import 'package:SmartDeviceDart/SmartDeviceDart.dart' as SmartDeviceDart;
 
-import 'package:firebase/firebase_io.dart';
-
 main(List<String> arguments) {
+
   print('Hello world: ${SmartDeviceDart.calculate()}!');
-
-  dataBase();
-
+  linuxCommand();
 }
+
 
 Future dataBase() async {
 
-  var credential = ''; // Retrieve auth credential
-  var fbClient = new FirebaseClient(credential); // FirebaseClient.anonymous() is also available
 
-  var path = ''; // Full path to your database location with .json appended
+//  var credential = ''; // Retrieve auth credential
+//  var fbClient = new FirebaseClient(); // FirebaseClient.anonymous() is also available
+//
+//  var path = ''; // Full path to your database location with .json appended
+//
+//  // GET
+//  var response = await fbClient.get(path);
+//
+//  // DELETE
+////  await fbClient.delete(path);
 
-  // GET
-  var response = await fbClient.get(path);
 
-  // DELETE
-//  await fbClient.delete(path);
+}
+
+void linuxCommand() async{
+  await Process.run('pwd', ['']).then((ProcessResult result){
+    print(result.stdout);
+  });
 
 }
