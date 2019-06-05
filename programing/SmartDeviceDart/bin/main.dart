597cd472f2b9a4359b9c380c8fb5d0366178bd28
Guy Luz <guyluz11@gmail.com>
2019-06-05 20:09:36 +0300

    Turn light command working

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -12,23 +12,16 @@ main(List<String> arguments) {
 Future dataBase() async {
 
 
-//  var credential = ''; // Retrieve auth credential
-//  var fbClient = new FirebaseClient(); // FirebaseClient.anonymous() is also available
-//
-//  var path = ''; // Full path to your database location with .json appended
-//
-//  // GET
-//  var response = await fbClient.get(path);
-//
-//  // DELETE
-////  await fbClient.delete(path);
-
-
 }
 
-void linuxCommand() async{
-  await Process.run('pwd', ['']).then((ProcessResult result){
+
+void linuxCommand() async {
+  await Process.run('gpio', ['mode', '7', 'out']).then((ProcessResult result){
     print(result.stdout);
   });
+  await Process.run('gpio', ['write', '7', '1']).then((ProcessResult result){
+    print(result.stdout);
+  });
+  print('I am done here');
 
 }
