c998dad539dcff24980e652b5493926f5d7bf7ba
Guy Luz <guyluz11@gmail.com>
2019-06-05 21:30:13 +0300

    New code to test

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -5,12 +5,20 @@ import 'package:SmartDeviceDart/SmartDeviceDart.dart' as SmartDeviceDart;
 main(List<String> arguments) {
 
   print('Hello world: ${SmartDeviceDart.calculate()}!');
+  dataBase();
+
   linuxCommand();
 }
 
 
 Future dataBase() async {
-
+  var fbClient = new FirebaseClient.anonymous();
+  String auth = 'vA8JV5FBKFUTiJNnODjc9s1V9y0N0yJvH2ST0WG0';
+  var path = "https://***REMOVED***.firebaseio.com/users/dani/name.json?auth=" + auth;
+  var response = await fbClient.get(path);
+  print(response);
+  // Delete field
+//  await fbClient.delete(path);
 
 }
 
@@ -18,10 +26,13 @@ Future dataBase() async {
 void linuxCommand() async {
   await Process.run('gpio', ['mode', '7', 'out']).then((ProcessResult result){
     print(result.stdout);
-  });
+  }).catchError(errorPrint);
   await Process.run('gpio', ['write', '7', '1']).then((ProcessResult result){
     print(result.stdout);
-  });
+  }).catchError(errorPrint);
   print('I am done here');
 
+}
+void errorPrint(Exception e){
+  print('Error is: ' + e.toString());
 }
