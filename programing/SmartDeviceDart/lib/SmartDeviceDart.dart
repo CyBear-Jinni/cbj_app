6221faf42a8acb16db529387b9f1f5280cacbbbe
Guy Luz <guyluz11@gmail.com>
2019-06-15 16:57:34 +0300

    Add basic NetworkManager, snap now smarter and download sdk

--- programing/SmartDeviceDart/lib/SmartDeviceDart.dart
+++ programing/SmartDeviceDart/lib/SmartDeviceDart.dart
@@ -1,5 +1,30 @@
-int calculate() {
-  return 6 * 7;
-}
+import 'package:rpi_gpio/gpio.dart';
+import 'package:rpi_gpio/rpi_gpio.dart';
 
+
+class SmartDevice {
+
+  RpiGpio gpio;
+
+  GpioOutput device;
+  SmartDevice(int gpioPin){
+    gpio = new RpiGpio();
+    device = gpio.output(gpioPin);
+
+
+  }
+
+  void TurnOn(){
+    device.value = true;
+  }
+
+  void TurnOff(){
+    device.value = false;
+  }
+
+  void Dispose(){
+    gpio.dispose();
+  }
+
+}
 
---
+++ snapSh/lightOn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Hello, World!" 
+
+echo "Done here"
+echo 
+echo $OLDPWD
+echo "Next"
+
+/snap/mainb/current/WiringNP/gpio/gpio 
+#python on.py
+
+
