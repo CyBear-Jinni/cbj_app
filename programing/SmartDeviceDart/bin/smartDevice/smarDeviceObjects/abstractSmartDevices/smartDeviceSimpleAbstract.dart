800d850959c62e9a3066190f2fc8246693169027
Guy Luz <guyluz11@gmail.com>
2019-06-16 23:32:19 +0300

    Add variables to abstract classes
    Former-commit-id: b574aa6c2736a417574d626b51d804fec82f7951

--- programing/SmartDeviceDart/bin/smartDevice/smarDeviceObjects/abstractSmartDevices/smartDeviceSimpleAbstract.dart
+++ programing/SmartDeviceDart/bin/smartDevice/smarDeviceObjects/abstractSmartDevices/smartDeviceSimpleAbstract.dart
@@ -2,6 +2,7 @@ import 'smartDeviceBaseAbstract.dart';
 
 // Abstract class for smart devices with on time property
 abstract class SmartDeviceSimple extends SmartDeviceBaseAbstract {
+  double howMuchTimeTheDeviceDoingAction;  // How much time the smart device was active (Doing action) continuously
 
   SmartDeviceSimple(String macAddress, String deviceName) : super(macAddress, deviceName);
 }
