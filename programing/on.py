2d2191441255ca724a8451471dd68fdaf2fd5cf2
Guy Luz <guyluz11@gmail.com>
2019-06-21 19:50:36 +0300

    Add dart command that execute the the python file

--- programing/on.py
+++ programing/on.py
@@ -1,6 +1,13 @@
+
+print ('0');
 import RPi.GPIO as GPIO
 
-pin = 7
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pin, GPIO.OUT)
-GPIO.output(pin, GPIO.HIGH)
+pin = 7;
+print ('1');
+GPIO.setmode(GPIO.BOARD);
+
+print ('2');
+GPIO.setup(pin, GPIO.OUT);
+
+print ('3');
+GPIO.output(pin, GPIO.HIGH);
