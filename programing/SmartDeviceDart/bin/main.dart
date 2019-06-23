a2645029d331bd0ad2f1f588ddab1bf10b5ef3b8
Guy Luz <guyluz11@gmail.com>
2019-06-23 23:50:45 +0300

    Removed rpi dart library, fix connection crash after multiple clicks
    Former-commit-id: 78973edb34c4b1f2d060135164f89a9161315894

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -3,6 +3,10 @@ import 'smartDevice/smartDeviceManager.dart';
 
 main(List<String> arguments) {
   print('Device is now ON');
-  Pythons.pythonFilesLocation = arguments[0];
+  try {
+    Pythons.pythonFilesLocation = arguments[0];
+  } catch (e) {
+    Pythons.pythonFilesLocation = '..';
+  }
   SmartDeviceMain();
 }
