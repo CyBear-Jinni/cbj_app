0a6141001fe4fd67217d504cca9f32adbb5098da
Guy Luz <guyluz11@gmail.com>
2019-06-23 20:20:15 +0300

    Working pin control from the app
    Former-commit-id: 907bee4838e5fcae1cdbb5497b53db802460b343

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -1,15 +1,8 @@
-import 'dart:io';
+import 'shered/pythons.dart';
+import 'smartDevice/smartDeviceManager.dart';
 
 main(List<String> arguments) {
   print('Device is now ON');
-  commandP(arguments[0]);
-  //SmartDeviceMain();
-}
-
-// function to start python script inside the path (snap location)
-void commandP(String path) async {
-  await Process.start('python3', [path + '/on.py']).then((process) {
-    stdout.addStream(process.stdout);
-    stderr.addStream(process.stderr);
-  });
+  Pythons.pythonFilesLocation = arguments[0];
+  SmartDeviceMain();
 }
