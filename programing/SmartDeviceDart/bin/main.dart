cce7d20462ab3b543b824a84ae525ee04141db19
Guy Luz <guyluz11@gmail.com>
2019-06-28 16:51:13 +0300

    Python code now execute from dart and not external file
    Former-commit-id: 2cd7e4b076dbaf92a6b5b2786f4fa865e48f9bff

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -1,12 +1,6 @@
-import 'shered/pythons.dart';
 import 'smartDevice/smartDeviceManager.dart';
 
 main(List<String> arguments) {
   print('Device is now ON');
-  try {
-    Pythons.pythonFilesLocation = arguments[0];
-  } catch (e) {
-    Pythons.pythonFilesLocation = '..';
-  }
   SmartDeviceMain();
 }
