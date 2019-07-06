c97efd71ee8bcf8ebc96cd9dd791e2384493261b
Guy Luz <guyluz11@gmail.com>
2019-07-06 23:58:27 +0300

    Add class to sand request to fire store

--- programing/SmartDeviceDart/bin/main.dart
+++ programing/SmartDeviceDart/bin/main.dart
@@ -1,6 +1,13 @@
 import 'smartDevice/smartDeviceManager.dart';
 
-main(List<String> arguments) {
+main(List<String> arguments) async {
   print('Device is now ON');
   SmartDeviceMain();
+//  await a();
+//  print('Progrm has ended');
 }
+
+//Future a() async {
+//  DataBaseController dataBaseController = new DataBaseController();
+//  print(await dataBaseController.getDataWhenChange('/users/aturing', 'first'));
+//}
