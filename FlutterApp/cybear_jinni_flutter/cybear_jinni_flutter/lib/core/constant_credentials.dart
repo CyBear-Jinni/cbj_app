ce69279abea0e209cb7924371742fb15d7e85a62
guy <guyluz11@gmail.com>
2020-09-07 12:42:57 +0300

    Fixing firestore not working after depenencie update
    Former-commit-id: 2d4ae6efe5c8fb62c6674bcece8aaa07bc070acf

---
+++ FlutterApp/cybear_jinni_flutter/cybear_jinni_flutter/lib/core/constant_credentials.dart
@@ -0,0 +1,10 @@
+// Disclaimer: this file is on .gitignore because it contains privet information
+// Contain the credentials that does not change
+
+class ConstantCredentials {
+// TODO: insert it from the firebase json
+  static const fireBaseProjectId = 'here';
+  static const fireBaseApiKey = 'here';
+  static const String userEmail = 'here';
+  static const String userPassword = 'here';
+}
