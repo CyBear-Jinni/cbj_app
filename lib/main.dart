59cddabf5c71b67ec838fac9d26d184d88f5e45e
Sagar Choudhary <sagar.choudhary96@gmail.com>
2020-10-03 23:53:22 +0530

    update: firebase initialize function to make use of futureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,19 @@ import 'package:flutter/material.dart';
 
 import 'features/home_page/home_page.dart';
 import 'features/login_page/login_page.dart';
+import 'features/shared_widgets/loader.dart';
+import 'features/shared_widgets/error_message.dart';
+import 'features/shared_widgets/loader.dart';
 
 void main() {
 //  debugPaintSizeEnabled = true;
   configureInjection(Env.prod);
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
-  Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   final Map routes = <String, WidgetBuilder>{
     LoginPage.tag: (BuildContext context) => LoginPage(),
 //    "HomePage": (context) => HomePage(),
@@ -24,42 +28,55 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Smart home',
-//      darkTheme: ThemeData(brightness: Brightness.dark),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        accentColor: Colors.indigo,
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-          bodyText2: TextStyle(color: Colors.white70),
-        ),
-        fontFamily: 'gidole_regular',
-      ),
-      routes: {
-        '/': (BuildContext context) => LoginPage(),
-        '/home': (BuildContext context) => HomePage(),
-//        '/home_settings': (BuildContext context) => SettingsPage(),
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        final List<String> pathElements = settings.name.split('/');
-        if (pathElements[0] != '') {
-          return null;
-        } else if (pathElements[1] == 'roomPage') {
-          return MaterialPageRoute(
-              builder: (BuildContext context) => RoomPage(rooms
-                  .firstWhere((SmartRoomObject room) =>
-                      room.getRoomName() == pathElements[2])
-                  .getRoomName()));
-        } else if (pathElements[1] == 'devices') {
-          if (pathElements[2] == 'blinds') {
-            return MaterialPageRoute(
-                builder: (BuildContext context) => BlindsPage());
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorMessage();
           }
-        }
-        return null;
-      },
-    );
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Smart home',
+              //  darkTheme: ThemeData(brightness: Brightness.dark),
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: Colors.deepPurple,
+                accentColor: Colors.indigo,
+                textTheme: const TextTheme(
+                  bodyText1: TextStyle(color: Colors.white),
+                  bodyText2: TextStyle(color: Colors.white70),
+                ),
+                fontFamily: 'gidole_regular',
+              ),
+              routes: {
+                '/': (BuildContext context) => LoginPage(),
+                '/home': (BuildContext context) => HomePage(),
+                // '/home_settings': (BuildContext context) => SettingsPage(),
+              },
+              onGenerateRoute: (RouteSettings settings) {
+                final List<String> pathElements = settings.name.split('/');
+                if (pathElements[0] != '') {
+                  return null;
+                } else if (pathElements[1] == 'roomPage') {
+                  return MaterialPageRoute(
+                      builder: (BuildContext context) => RoomPage(rooms
+                          .firstWhere((SmartRoomObject room) =>
+                      room.getRoomName() == pathElements[2])
+                          .getRoomName()));
+                } else if (pathElements[1] == 'devices') {
+                  if (pathElements[2] == 'blinds') {
+                    return MaterialPageRoute(
+                        builder: (BuildContext context) => BlindsPage());
+                  }
+                }
+                return null;
+              },
+            );
+          }
+
+          return Loader();
+        });
   }
 }
