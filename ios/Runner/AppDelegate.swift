6da9ec618b3a57a8ea9465c8a81c027fbaf4c3a0
Guy Luz <guyluz11@gmail.com>
2021-01-30 21:39:05 +0200

    Next step running on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,45 @@
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  var window: UIWindow?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
+    if(!UserDefaults.standard.bool(forKey: "Notification")) {
+        UIApplication.shared.cancelAllLocalNotifications()
+        UserDefaults.standard.set(true, forKey: "Notification")
+    }
+
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "dexterx.dev/flutter_local_notifications_example",
+                                        binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        if ("getTimeZoneName" == call.method) {
+            result(TimeZone.current.identifier)
+        }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+``
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
+
