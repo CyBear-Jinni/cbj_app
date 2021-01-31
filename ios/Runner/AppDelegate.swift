0e37ace5749d71334d0f697ac31a6027ee05850b
Guy Luz <guyluz11@gmail.com>
2021-01-31 04:53:30 +0200

    App is now lunching on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,10 +1,10 @@
 import UIKit
 import Flutter
+import Firebase
 
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  var window: UIWindow?
 
   override func application(
     _ application: UIApplication,
@@ -14,32 +14,17 @@ import Flutter
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
 
+
     if(!UserDefaults.standard.bool(forKey: "Notification")) {
         UIApplication.shared.cancelAllLocalNotifications()
         UserDefaults.standard.set(true, forKey: "Notification")
     }
 
-
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name: "dexterx.dev/flutter_local_notifications_example",
-                                        binaryMessenger: controller.binaryMessenger)
-    channel.setMethodCallHandler({
-        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-        if ("getTimeZoneName" == call.method) {
-            result(TimeZone.current.identifier)
-        }
-    })
+    FirebaseApp.configure()
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-``
-  func application(_ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions:
-      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
 }
 
