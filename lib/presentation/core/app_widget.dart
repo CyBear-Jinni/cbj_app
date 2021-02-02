eb1aa749f904517959dbc6993fc4be4d9f9c70d1
Guy Luz <guyluz11@gmail.com>
2021-02-02 16:31:41 +0200

    Removed old commented code

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cybear_jinni/application/auth/auth_bloc.dart';
 import 'package:cybear_jinni/injection.dart';
 import 'package:cybear_jinni/presentation/routes/app_router.gr.dart';
-import 'package:cybear_jinni/presentation/sign_in_page/login_page.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +9,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppWidget extends StatelessWidget {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  final Map routes = <String, WidgetBuilder>{
-    SignInPage.tag: (BuildContext context) => SignInPage(),
-//    "HomePage": (context) => HomePage(),
-  };
 
   @override
   Widget build(BuildContext context) {
@@ -51,38 +46,6 @@ class AppWidget extends StatelessWidget {
           ),
           fontFamily: 'gidole_regular',
         ),
-        //
-        // routes: <String, WidgetBuilder>{
-        //   '/$loginPage': (BuildContext context) => SignInPage(),
-        //   '/$homePage': (BuildContext context) => HomePage(),
-        //   //        '/home_settings': (BuildContext context) => SettingsPage(),
-        // },
-        // onGenerateRoute: (RouteSettings settings) {
-        //   final List<String> pathElements = settings.name.split('/');
-        //   if (pathElements[0] != '') {
-        //     return null;
-        //   } else if (pathElements[1] == lightsInTheRoomPage) {
-        //     return MaterialPageRoute(
-        //         builder: (BuildContext context) => LightsInTheRoomPage(
-        //             rooms
-        //                 .firstWhere((SmartRoomObject room) =>
-        //             room.getRoomName() == pathElements[2])
-        //                 .getRoomName()));
-        //   } else if (pathElements[1] == devicesPage) {
-        //     if (pathElements[2] == blindsPage.tr()) {
-        //       return MaterialPageRoute(
-        //           builder: (BuildContext context) => BlindsPage());
-        //     } else if (pathElements[2] == lightsPage.tr()) {
-        //       return MaterialPageRoute(
-        //           builder: (BuildContext context) => LightsPage());
-        //     }
-        //   } else if (pathElements[1] == whereToLoginPage) {
-        //     return MaterialPageRoute(
-        //         settings: settings,
-        //         builder: (BuildContext context) => WhereToLoginPage());
-        //   }
-        //   return null;
-        // },
       ),
     );
   }
