26bac2157e4db2fcfc37c89edea4392108a1e13c
guy <guyluz11@gmail.com>
2021-09-30 09:55:21 +0300

    Changing liquid_progress_indicator package back to original and not fork as it got sound null safety.

--- lib/presentation/add_new_devices_process/configure_new_cbj_comp/widgets/configure_new_cbj_comp_widget.dart
+++ lib/presentation/add_new_devices_process/configure_new_cbj_comp/widgets/configure_new_cbj_comp_widget.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:liquid_progress_indicator_ns/liquid_progress_indicator.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
 class ConfigureNewCbjCompWidgets extends StatelessWidget {
   const ConfigureNewCbjCompWidgets({
