a979e22182fc499f08bb3bc3a424566bf26f31d6
Guy Luz <guyluz11@gmail.com>
2021-12-22 12:51:01 +0200

    Added border to device type name in rooms for visibility and focus

--- lib/presentation/home_page/tabs/smart_devices_tab/devices_in_the_room_blocks/blinds_in_the_room.dart
+++ lib/presentation/home_page/tabs/smart_devices_tab/devices_in_the_room_blocks/blinds_in_the_room.dart
@@ -115,12 +115,26 @@ class BlindsInTheRoom extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 5),
-            AutoSizeText(
-              deviceText,
-              maxLines: 1,
-              style: TextStyle(
-                color: Theme.of(context).textTheme.bodyText1!.color,
-              ),
+            Stack(
+              children: <Widget>[
+                AutoSizeText(
+                  deviceText,
+                  maxLines: 1,
+                  style: TextStyle(
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 0.8
+                      ..color = Colors.black38,
+                  ),
+                ),
+                AutoSizeText(
+                  deviceText,
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.bodyText1!.color,
+                  ),
+                ),
+              ],
             ),
             const SizedBox(
               height: 10,
