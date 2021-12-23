6f9f179b980ba7efb2cdbcf9fb936325d2b58fc6
guyluz11 <guyluz11@gmail.com>
2021-12-23 03:10:26 +0200

    Reverted back to targetSdkVersion 30, small UI color fixes

--- lib/presentation/add_vendors_pages/add_jinvoo_smart_vendor/widgets/jinvoo_smart_sign_in_form.dart
+++ lib/presentation/add_vendors_pages/add_jinvoo_smart_vendor/widgets/jinvoo_smart_sign_in_form.dart
@@ -6,7 +6,6 @@ import 'package:cybear_jinni/infrastructure/core/gen/cbj_hub_server/protoc_as_da
 import 'package:cybear_jinni/presentation/routes/app_router.gr.dart';
 import 'package:dartz/dartz.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -200,12 +199,11 @@ class JinvooSmartSignInForm extends StatelessWidget {
                                   );
 
                               Fluttertoast.showToast(
-                                msg:
-                                    'Sign in to Jinvoo Smart, please restart the '
-                                    'app to see the new devices',
+                                msg: 'Sign in to Jinvoo Smart, please restart '
+                                    'the app to see the new devices',
                                 toastLength: Toast.LENGTH_LONG,
                                 gravity: ToastGravity.BOTTOM,
-                                backgroundColor: Colors.deepOrange,
+                                backgroundColor: Colors.indigo,
                                 textColor: Theme.of(context)
                                     .textTheme
                                     .bodyText1!
