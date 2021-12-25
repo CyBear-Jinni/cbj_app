62304ea9a2eab7c3e56d6e2cc9f25530ce1d10d9
guyluz11 <guyluz11@gmail.com>
2021-12-25 20:01:54 +0200

    Fixed hot reload not working and creating white screen

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppWidget extends StatelessWidget {
-  final _rootRouter = AppRouter();
+  static final _rootRouter = AppRouter();
 
   MaterialColor createMaterialColor(Color color) {
     final List<double> strengths = <double>[.05];
