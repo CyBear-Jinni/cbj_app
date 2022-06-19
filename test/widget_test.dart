e4c0f8844aef35dbe273ba36378fab50b95bfb74
guyluz11 <guyluz11@gmail.com>
2022-06-19 15:13:53 +0300

    Removed testing dependency for now as we don't use it and it does not use the last version of async

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,28 +1,28 @@
-// This is a basic Flutter widgets test.
+// // This is a basic Flutter widgets test.
+// //
+// // To perform an interaction with a widgets in your test, use the WidgetTester
+// // utility that Flutter provides. For example, you can send tap and scroll
+// // gestures. You can also use WidgetTester to find child widgets in the widgets
+// // tree, read text, and verify that the values of widgets properties are correct.
 //
-// To perform an interaction with a widgets in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widgets
-// tree, read text, and verify that the values of widgets properties are correct.
-
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // // Build our app and trigger a frame.
-    // await tester.pumpWidget(AppWidget());
-    //
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-    //
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-    //
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
-    expect(1, 1);
-  });
-}
+// import 'package:flutter_test/flutter_test.dart';
+//
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // // Build our app and trigger a frame.
+//     // await tester.pumpWidget(AppWidget());
+//     //
+//     // // Verify that our counter starts at 0.
+//     // expect(find.text('0'), findsOneWidget);
+//     // expect(find.text('1'), findsNothing);
+//     //
+//     // // Tap the '+' icon and trigger a frame.
+//     // await tester.tap(find.byIcon(Icons.add));
+//     // await tester.pump();
+//     //
+//     // // Verify that our counter has incremented.
+//     // expect(find.text('0'), findsNothing);
+//     // expect(find.text('1'), findsOneWidget);
+//     expect(1, 1);
+//   });
+// }
