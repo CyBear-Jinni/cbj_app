7dd90d9caa79fb000f122877782ed64e457a0bde
guyluz11 <guyluz11@gmail.com>
2022-06-19 16:39:47 +0300

    Added some dart test so that the github action will not fail (as I remove the empty flutter test)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -26,3 +26,15 @@
 //     expect(1, 1);
 //   });
 // }
+
+import 'package:test/test.dart';
+
+void main() {
+  test('DST example Test 2', () {
+    final DateTime today = DateTime(2020, 3, 8);
+    final DateTime tomorrow = DateTime(2020, 3, 9);
+    final diff = tomorrow.difference(today);
+    final int days = diff.inDays;
+    expect(days, 1);
+  });
+}
