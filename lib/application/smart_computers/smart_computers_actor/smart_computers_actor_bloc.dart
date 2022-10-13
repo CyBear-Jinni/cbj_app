4858538e6f290af1fc226ebc3aa8439f7d82be24
guyluz11 <guyluz11@gmail.com>
2022-10-13 22:12:37 +0300

    Completed (suspend) request from the App to Hub to Cbj Smart Computer

---
+++ lib/application/smart_computers/smart_computers_actor/smart_computers_actor_bloc.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:another_flushbar/flushbar_helper.dart';
+import 'package:bloc/bloc.dart';
+import 'package:cybear_jinni/domain/devices/device/devices_failures.dart';
+import 'package:cybear_jinni/domain/devices/device/i_device_repository.dart';
+import 'package:cybear_jinni/domain/devices/generic_switch_device/generic_switch_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+part 'smart_computers_actor_bloc.freezed.dart';
+part 'smart_computers_actor_event.dart';
+part 'smart_computers_actor_state.dart';
+
+@injectable
+class SmartComputersActorBloc
+    extends Bloc<SmartComputersActorEvent, SmartComputersActorState> {
+  SmartComputersActorBloc(this._deviceRepository)
+      : super(const SmartComputersActorState.initial()) {
+    on<Initialized>(_initialized);
+    on<Deleted>(_deleted);
+    on<TurnOffAllSmartComputers>(_turnOffAllSmartComputers);
+    on<TurnOnAllSmartComputers>(_turnOnAllSmartComputers);
+    on<SuspendAllSmartComputers>(_suspendAllSmartComputers);
+  }
+
+  final IDeviceRepository _deviceRepository;
+
+  Future<void> _initialized(
+    Initialized event,
+    Emitter<SmartComputersActorState> emit,
+  ) async {}
+
+  Future<void> _deleted(
+    Deleted event,
+    Emitter<SmartComputersActorState> emit,
+  ) async {}
+
+  Future<void> _turnOffAllSmartComputers(
+    TurnOffAllSmartComputers event,
+    Emitter<SmartComputersActorState> emit,
+  ) async {
+    FlushbarHelper.createLoading(
+      message: 'Turning Off all Smart Computers',
+      linearProgressIndicator: const LinearProgressIndicator(),
+    ).show(event.context);
+
+    _deviceRepository.turnOffDevices(
+      devicesId: event.smartComputersIdToTurnOff,
+    );
+  }
+
+  Future<void> _turnOnAllSmartComputers(
+    TurnOnAllSmartComputers event,
+    Emitter<SmartComputersActorState> emit,
+  ) async {
+    FlushbarHelper.createLoading(
+      message: 'Turning On all Smart Computers',
+      linearProgressIndicator: const LinearProgressIndicator(),
+    ).show(event.context);
+
+    _deviceRepository.turnOnDevices(devicesId: event.smartComputersIdToTurnOn);
+  }
+
+  Future<void> _suspendAllSmartComputers(
+    SuspendAllSmartComputers event,
+    Emitter<SmartComputersActorState> emit,
+  ) async {
+    FlushbarHelper.createLoading(
+      message: 'Suspending all Smart Computers',
+      linearProgressIndicator: const LinearProgressIndicator(),
+    ).show(event.context);
+
+    _deviceRepository.suspendDevices(devicesId: event.smartComputersIdToTurnOn);
+  }
+}
--- lib/presentation/device_full_screen_page/smart_computers/rooms_smart_computers_page.dart
+++ lib/presentation/device_full_screen_page/smart_computers/rooms_smart_computers_page.dart
@@ -2,28 +2,29 @@ import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:cybear_jinni/application/auth/auth_bloc.dart';
 import 'package:cybear_jinni/application/devices/device_actor/device_actor_bloc.dart';
-import 'package:cybear_jinni/application/smart_plugs/smart_plugs_watcher/smart_plugs_watcher_bloc.dart';
+import 'package:cybear_jinni/application/smart_computers/smart_computers_watcher/smart_computer_watcher_bloc.dart';
 import 'package:cybear_jinni/domain/room/room_entity.dart';
 import 'package:cybear_jinni/injection.dart';
 import 'package:cybear_jinni/presentation/core/theme_data.dart';
-import 'package:cybear_jinni/presentation/device_full_screen_page/smart_plugs/settings_page_of_smart_plug.dart';
-import 'package:cybear_jinni/presentation/device_full_screen_page/smart_plugs/widgets/rooms_smart_plugs_widget.dart';
+import 'package:cybear_jinni/presentation/device_full_screen_page/smart_computers/settings_page_of_smart_computers.dart';
+import 'package:cybear_jinni/presentation/device_full_screen_page/smart_computers/widgets/rooms_smart_computers_widget.dart';
 import 'package:cybear_jinni/presentation/routes/app_router.gr.dart';
 import 'package:cybear_jinni/presentation/shared_widgets/top_navigation_bar.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-/// SmartPlugs page that will call the lamps widgets file and add background
+/// SmartComputers page that will call the lamps widgets file and add background
 /// and scaffold for it
-class RoomsSmartPlugsPage extends StatelessWidget {
-  const RoomsSmartPlugsPage({
+class RoomsSmartComputersPage extends StatelessWidget {
+  const RoomsSmartComputersPage({
     required this.roomEntity,
     this.roomColorGradiant,
   });
 
-  /// If it have value will only show smartPlugs in this room
+  /// If it have value will only show SmartComputers in this room
   final RoomEntity roomEntity;
   final List<Color>? roomColorGradiant;
 
@@ -31,7 +32,7 @@ class RoomsSmartPlugsPage extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (BuildContext context) => SettingsPageOfSmartPlugs(
+        builder: (BuildContext context) => SettingsPageOfSmartComputers(
           roomEntity: roomEntity,
         ),
       ),
@@ -46,9 +47,9 @@ class RoomsSmartPlugsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<SmartPlugsWatcherBloc>(
-          create: (context) => getIt<SmartPlugsWatcherBloc>()
-            ..add(const SmartPlugsWatcherEvent.watchAllStarted()),
+        BlocProvider<SmartComputersWatcherBloc>(
+          create: (context) => getIt<SmartComputersWatcherBloc>()
+            ..add(const SmartComputersWatcherEvent.watchAllStarted()),
         ),
         BlocProvider<DeviceActorBloc>(
           create: (context) => getIt<DeviceActorBloc>(),
@@ -93,14 +94,14 @@ class RoomsSmartPlugsPage extends StatelessWidget {
             backgroundColor: Colors.black,
             systemOverlayStyle: SystemUiOverlayStyle.dark,
           ),
-          body: Container(
+          body: DecoratedBox(
             decoration: BoxDecoration(
               gradient: BacgroundGradient.getBacground(context),
             ),
             child: Column(
               children: [
                 TopNavigationBar(
-                  pageName: 'SmartPlugs',
+                  pageName: 'Smart Computers'.tr(),
                   rightIcon: null,
                   rightIconFunction: cogFunction,
                   leftIcon: FontAwesomeIcons.arrowLeft,
@@ -108,7 +109,7 @@ class RoomsSmartPlugsPage extends StatelessWidget {
                   backgroundColor: roomColorGradiant!.last,
                 ),
                 Expanded(
-                  child: RoomsSmartPlugsWidget(
+                  child: RoomsSmartComputersWidget(
                     roomEntity,
                     roomColorGradiant!,
                   ),
