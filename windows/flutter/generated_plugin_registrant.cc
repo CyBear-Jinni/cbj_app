3e4b604b4da78caa3419d5925386c211de2c6b29
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2022-10-23 18:23:05 +0530

    revert wrong changes

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,8 +6,8 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus/connectivity_plus_windows_plugin.h>
-#include <network_info_plus/network_info_plus_windows_plugin.h>
+#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <network_info_plus_windows/network_info_plus_windows_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
