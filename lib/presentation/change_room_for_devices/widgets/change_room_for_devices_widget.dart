06c668bc39f0d46778cfe3cc8c0b14f1dc30dffd
Sanketh B K <51091231+SankethBK@users.noreply.github.com>
2022-11-04 00:38:49 +0530

    added responsive layout for add devices page

--- lib/presentation/change_room_for_devices/widgets/change_room_for_devices_widget.dart
+++ lib/presentation/change_room_for_devices/widgets/change_room_for_devices_widget.dart
@@ -14,123 +14,122 @@ class ChangeRoomForDevicesWidget extends StatelessWidget {
     return BlocConsumer<RoomSignInFormBloc, RoomSignInFormState>(
       listener: (context, state) {},
       builder: (context, state) {
-        return SingleChildScrollView(
-          child: Container(
-            color: HexColor('#3A3A3A'),
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Choose Area',
-                      style: TextStyle(fontSize: 27),
+        return Container(
+          color: HexColor('#3A3A3A'),
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              SizedBox(
+                height: screenSize.height * 0.05,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Choose Area',
+                    style: TextStyle(fontSize: 27),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      context.router.push(const AddNewRoomRoute());
+                    },
+                    child: const Text(
+                      'Add New Area',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        backgroundColor: Colors.blue,
+                      ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        context.router.push(const AddNewRoomRoute());
-                      },
-                      child: const Text(
-                        'Add New Area',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          backgroundColor: Colors.blue,
+                  ),
+                ],
+              ),
+              DropdownButton<String>(
+                dropdownColor: Colors.black,
+                style: const TextStyle(color: Colors.white),
+                icon: const Icon(Icons.arrow_drop_down),
+                hint: Text(
+                  state.defaultName.isValid()
+                      ? state.defaultName.getOrCrash()
+                      : 'Choose Area',
+                  style: const TextStyle(color: Colors.white),
+                ),
+                elevation: 16,
+                underline: Container(
+                  height: 2,
+                ),
+                onChanged: (value) {
+                  context
+                      .read<RoomSignInFormBloc>()
+                      .add(RoomSignInFormEvent.roomIdChanged(value!));
+                },
+                items: state.allRooms.map<DropdownMenuItem<String>>((e) {
+                  //     .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: e.uniqueId.getOrCrash(),
+                    child: Text(e.defaultName.getOrCrash()),
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: screenSize.height * 0.1,
+              ),
+              Row(
+                children: const [
+                  Text(
+                    'Choose Device',
+                    style: TextStyle(fontSize: 27),
+                  ),
+                ],
+              ),
+              Container(
+                constraints: BoxConstraints(maxHeight: screenSize.height * 0.5),
+                child: SingleChildScrollView(
+                  child: MultiSelectDialogField(
+                    items: List<MultiSelectItem<String?>>.of(
+                      state.allDevices.map(
+                        (e) => MultiSelectItem(
+                          e.uniqueId.getOrCrash(),
+                          e.defaultName.getOrCrash()!,
                         ),
                       ),
                     ),
-                  ],
-                ),
-                DropdownButton<String>(
-                  dropdownColor: Colors.black,
-                  style: const TextStyle(color: Colors.white),
-                  icon: const Icon(Icons.arrow_drop_down),
-                  hint: Text(
-                    state.defaultName.isValid()
-                        ? state.defaultName.getOrCrash()
-                        : 'Choose Area',
-                    style: const TextStyle(color: Colors.white),
+                    listType: MultiSelectListType.CHIP,
+                    onConfirm: (List<String?> values) {
+                      context.read<RoomSignInFormBloc>().add(
+                            RoomSignInFormEvent.roomDevicesIdChanged(
+                              List<String>.from(values),
+                            ),
+                          );
+                    },
                   ),
-                  elevation: 16,
-                  underline: Container(
-                    height: 2,
-                  ),
-                  onChanged: (value) {
-                    context
-                        .read<RoomSignInFormBloc>()
-                        .add(RoomSignInFormEvent.roomIdChanged(value!));
-                  },
-                  items: state.allRooms.map<DropdownMenuItem<String>>((e) {
-                    //     .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: e.uniqueId.getOrCrash(),
-                      child: Text(e.defaultName.getOrCrash()),
-                    );
-                  }).toList(),
                 ),
-                const SizedBox(
-                  height: 70,
-                ),
-                Row(
-                  children: const [
-                    Text(
-                      'Choose Device',
-                      style: TextStyle(fontSize: 27),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: 'Changing devices for area',
+                        toastLength: Toast.LENGTH_LONG,
+                        gravity: ToastGravity.BOTTOM,
+                        backgroundColor: Colors.purple,
+                        textColor: Theme.of(context).textTheme.bodyText1!.color,
+                        fontSize: 16.0,
+                      );
+                      context.read<RoomSignInFormBloc>().add(
+                            RoomSignInFormEvent.changeRoomDevices(context),
+                          );
+                    },
+                    child: const Text(
+                      'Done',
+                      style: TextStyle(fontSize: 20),
                     ),
-                  ],
-                ),
-                MultiSelectDialogField(
-                  items: List<MultiSelectItem<String?>>.of(
-                    state.allDevices.map(
-                      (e) => MultiSelectItem(
-                        e.uniqueId.getOrCrash(),
-                        e.defaultName.getOrCrash()!,
-                      ),
-                    ),
-                  ),
-                  listType: MultiSelectListType.CHIP,
-                  onConfirm: (List<String?> values) {
-                    context.read<RoomSignInFormBloc>().add(
-                          RoomSignInFormEvent.roomDevicesIdChanged(
-                            List<String>.from(values),
-                          ),
-                        );
-                  },
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Fluttertoast.showToast(
-                          msg: 'Changing devices for area',
-                          toastLength: Toast.LENGTH_LONG,
-                          gravity: ToastGravity.BOTTOM,
-                          backgroundColor: Colors.purple,
-                          textColor:
-                              Theme.of(context).textTheme.bodyText1!.color,
-                          fontSize: 16.0,
-                        );
-                        context.read<RoomSignInFormBloc>().add(
-                              const RoomSignInFormEvent.changeRoomDevices(),
-                            );
-                      },
-                      child: const Text(
-                        'Done',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
+                  )
+                ],
+              )
+            ],
           ),
         );
       },
