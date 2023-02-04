887397ad8c5c4f5466721444dcd3676f1d8ed010
guyluz11 <guyluz11@gmail.com>
2023-02-04 16:23:41 +0200

    Adding light with brightness device type

---
+++ lib/infrastructure/generic_devices/generic_light_with_brightness_device/generic_light_with_brightness_device_dtos.dart
@@ -0,0 +1,88 @@
+import 'package:cybear_jinni/domain/devices/abstract_device/device_entity_abstract.dart';
+import 'package:cybear_jinni/domain/devices/abstract_device/value_objects_core.dart';
+import 'package:cybear_jinni/domain/devices/generic_light_with_brightness_device/generic_light_with_brightness_entity.dart';
+import 'package:cybear_jinni/domain/devices/generic_light_with_brightness_device/generic_light_with_brightness_value_objects.dart';
+import 'package:cybear_jinni/infrastructure/generic_devices/abstract_device/device_entity_dto_abstract.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'generic_light_with_brightness_device_dtos.freezed.dart';
+part 'generic_light_with_brightness_device_dtos.g.dart';
+
+@freezed
+abstract class GenericLightWithBrightnessDeviceDtos
+    implements _$GenericLightWithBrightnessDeviceDtos, DeviceEntityDtoAbstract {
+  factory GenericLightWithBrightnessDeviceDtos({
+    // @JsonKey(ignore: true)
+    required String id,
+    required String vendorUniqueId,
+    required String? defaultName,
+    required String? deviceStateGRPC,
+    required String? senderDeviceOs,
+    required String? senderDeviceModel,
+    required String? senderId,
+    required String? lightSwitchState,
+    required String? deviceTypes,
+    required String? compUuid,
+    required String? deviceVendor,
+    required String? lightBrightness,
+    String? deviceDtoClass,
+    String? stateMassage,
+
+    // required ServerTimestampConverter() FieldValue serverTimeStamp,
+  }) = _GenericLightWithBrightnessDeviceDtos;
+
+  GenericLightWithBrightnessDeviceDtos._();
+
+  factory GenericLightWithBrightnessDeviceDtos.fromDomain(
+    GenericLightWithBrightnessDE genericLightWithBrightnessDE,
+  ) {
+    return GenericLightWithBrightnessDeviceDtos(
+      deviceDtoClass: (GenericLightWithBrightnessDeviceDtos).toString(),
+      id: genericLightWithBrightnessDE.uniqueId.getOrCrash(),
+      vendorUniqueId: genericLightWithBrightnessDE.vendorUniqueId.getOrCrash(),
+      defaultName: genericLightWithBrightnessDE.defaultName.getOrCrash(),
+      deviceStateGRPC:
+          genericLightWithBrightnessDE.deviceStateGRPC.getOrCrash(),
+      stateMassage: genericLightWithBrightnessDE.stateMassage.getOrCrash(),
+      senderDeviceOs: genericLightWithBrightnessDE.senderDeviceOs.getOrCrash(),
+      senderDeviceModel:
+          genericLightWithBrightnessDE.senderDeviceModel.getOrCrash(),
+      senderId: genericLightWithBrightnessDE.senderId.getOrCrash(),
+      lightSwitchState:
+          genericLightWithBrightnessDE.lightSwitchState!.getOrCrash(),
+      deviceTypes: genericLightWithBrightnessDE.deviceTypes.getOrCrash(),
+      compUuid: genericLightWithBrightnessDE.compUuid.getOrCrash(),
+      deviceVendor: genericLightWithBrightnessDE.deviceVendor.getOrCrash(),
+      lightBrightness:
+          genericLightWithBrightnessDE.lightBrightness.getOrCrash(),
+
+      // serverTimeStamp: FieldValue.serverTimestamp(),
+    );
+  }
+
+  factory GenericLightWithBrightnessDeviceDtos.fromJson(
+          Map<String, dynamic> json) =>
+      _$GenericLightWithBrightnessDeviceDtosFromJson(json);
+
+  @override
+  final String deviceDtoClassInstance =
+      (GenericLightWithBrightnessDeviceDtos).toString();
+
+  @override
+  DeviceEntityAbstract toDomain() {
+    return GenericLightWithBrightnessDE(
+      uniqueId: CoreUniqueId.fromUniqueString(id),
+      vendorUniqueId: VendorUniqueId.fromUniqueString(vendorUniqueId),
+      defaultName: DeviceDefaultName(defaultName),
+      deviceStateGRPC: DeviceState(deviceStateGRPC),
+      stateMassage: DeviceStateMassage(stateMassage),
+      senderDeviceOs: DeviceSenderDeviceOs(senderDeviceOs),
+      senderDeviceModel: DeviceSenderDeviceModel(senderDeviceModel),
+      senderId: DeviceSenderId.fromUniqueString(senderId),
+      deviceVendor: DeviceVendor(deviceVendor),
+      compUuid: DeviceCompUuid(compUuid),
+      lightSwitchState: GenericLightWithBrightnessSwitchState(lightSwitchState),
+      lightBrightness: GenericLightBrightnessBrightness(lightBrightness),
+    );
+  }
+}
