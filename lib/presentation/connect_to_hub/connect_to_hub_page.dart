9897ee04e72e1d84889e37daf966b49bfe43e208
Manthan Khandale <manthan@kisan.net>
2023-10-02 15:00:46 +0530

    style: Connect to Hub page UI updated

--- lib/presentation/connect_to_hub/connect_to_hub_page.dart
+++ lib/presentation/connect_to_hub/connect_to_hub_page.dart
@@ -18,152 +18,91 @@ class ConnectToHubPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 0,
-        backgroundColor: HexColor('#303030'),
         systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
-      backgroundColor: HexColor('#303030'),
       //added CustomScrollView to solve pixel overflow
-      body: CustomScrollView(
-        slivers: [
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 30),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        Container(
-                          alignment: Alignment.center,
-                          child: const Text(
-                            ' Connect to Hub',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 35),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        Expanded(
-                          child: ColoredBox(
-                            color: HexColor('#3A3A3A'),
-                            child: Column(
-                              children: [
-                                const SizedBox(
-                                  height: 15,
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.symmetric(
-                                    horizontal: 15,
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        color: HexColor('#F8F9FA'),
-                                        width: 40,
-                                        height: 40,
-                                        child: Center(
-                                          child: FaIcon(
-                                            FontAwesomeIcons.networkWired,
-                                            color: HexColor('#495057'),
-                                          ),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        width: 40,
-                                      ),
-                                      Flexible(
-                                        child: Text(
-                                          'Search a CyBear Jinni Hub in your network',
-                                          style: TextStyle(
-                                            color: HexColor('#D6D6D6'),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Center(
-                                    child: BlocProvider(
-                                      create: (BuildContext context) =>
-                                          getIt<HubInNetworkBloc>()
-                                            ..add(
-                                              HubInNetworkEvent.initialEvent(
-                                                context,
-                                              ),
-                                            ),
-                                      child: CbjHubInNetworkWidget(),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: screenSize.height / 20,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    context.router.push(const ConnectToHomeWifiRoute());
-
-                    // Fluttertoast.showToast(
-                    //   msg: 'Connecting Hub to wireless network is not supported',
-                    //   toastLength: Toast.LENGTH_SHORT,
-                    //   gravity: ToastGravity.SNACKBAR,
-                    //   backgroundColor: Colors.lightBlue,
-                    //   textColor: Theme.of(context).textTheme.bodyLarge!.color,
-                    //   fontSize: 16.0,
-                    // );
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 30),
-                    height: 60,
-                    color: HexColor('#3A3A3A'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              HexColor('#9355e5'),
+              HexColor('#b162e4'),
+            ],
+          ),
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                children: [
+                  Expanded(
                     child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Row(
+                      margin: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
                         children: [
+                          const SizedBox(
+                            height: 50,
+                          ),
                           Container(
-                            color: HexColor('#F8F9FA'),
-                            width: 40,
-                            height: 40,
-                            child: Center(
-                              child: FaIcon(
-                                FontAwesomeIcons.wifi,
-                                color: HexColor('#495057'),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              ' Connect to Hub',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 35,
+                                color: Colors.white,
                               ),
                             ),
                           ),
                           const SizedBox(
-                            width: 40,
+                            height: 40,
                           ),
-                          Flexible(
-                            child: RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                  color: Colors.white.withOpacity(0.9),
-                                ),
-                                /*defining default style is optional */
-                                children: <TextSpan>[
-                                  const TextSpan(
-                                    text: 'Wireless Network\n',
-                                    style: TextStyle(fontSize: 16),
+                          Expanded(
+                            child: ColoredBox(
+                              color: HexColor('#985dc7'),
+                              child: Column(
+                                children: [
+                                  const SizedBox(
+                                    height: 15,
                                   ),
-                                  TextSpan(
-                                    text: 'Connect a CyBear Jinni Hub to WiFi '
-                                        'network',
-                                    style:
-                                        TextStyle(color: HexColor('#D6D6D6')),
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(
+                                      horizontal: 15,
+                                    ),
+                                    child: const Row(
+                                      children: [
+                                        FaIcon(
+                                          FontAwesomeIcons.networkWired,
+                                          color: Colors.white,
+                                        ),
+                                        SizedBox(
+                                          width: 40,
+                                        ),
+                                        Flexible(
+                                          child: Text(
+                                            'Search a CyBear Jinni Hub in your network',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Center(
+                                      child: BlocProvider(
+                                        create: (BuildContext context) =>
+                                            getIt<HubInNetworkBloc>()
+                                              ..add(
+                                                HubInNetworkEvent.initialEvent(
+                                                  context,
+                                                ),
+                                              ),
+                                        child: CbjHubInNetworkWidget(),
+                                      ),
+                                    ),
                                   ),
                                 ],
                               ),
@@ -173,35 +112,95 @@ class ConnectToHubPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () async {
-                          context.router.push(const ConnectToHubMoreRoute());
-                        },
-                        style: TextButton.styleFrom(
-                          minimumSize: const Size(70, 30),
-                          backgroundColor: HexColor('#858585'),
-                        ),
-                        child: const Text(
-                          'More',
-                          style: TextStyle(color: Colors.white),
+                  SizedBox(
+                    height: screenSize.height / 20,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      context.router.push(const ConnectToHomeWifiRoute());
+
+                      // Fluttertoast.showToast(
+                      //   msg: 'Connecting Hub to wireless network is not supported',
+                      //   toastLength: Toast.LENGTH_SHORT,
+                      //   gravity: ToastGravity.SNACKBAR,
+                      //   backgroundColor: Colors.lightBlue,
+                      //   textColor: Theme.of(context).textTheme.bodyLarge!.color,
+                      //   fontSize: 16.0,
+                      // );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 30),
+                      height: 60,
+                      color: HexColor('#985dc7'),
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            const FaIcon(
+                              FontAwesomeIcons.wifi,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 40,
+                            ),
+                            Flexible(
+                              child: RichText(
+                                text: TextSpan(
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.9),
+                                  ),
+                                  /*defining default style is optional */
+                                  children: const <TextSpan>[
+                                    TextSpan(
+                                      text: 'Wireless Network\n',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                    TextSpan(
+                                      text:
+                                          'Connect a CyBear Jinni Hub to WiFi '
+                                          'network',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () async {
+                            context.router.push(const ConnectToHubMoreRoute());
+                          },
+                          style: TextButton.styleFrom(
+                            minimumSize: const Size(70, 30),
+                            backgroundColor: HexColor('#985dc7'),
+                          ),
+                          child: const Text(
+                            'More',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
